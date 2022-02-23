e4000daffcba54bf37c0e7c88072635f099224c6
TheBassemNabil <bass.mar111@outlook.com>
2022-02-23 21:10:59 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { NgCarouselModule } from 'angular-custom-carousel';
 import { AppComponent } from './app.component';
 import { CarouselDemoComponent } from './carouselDemo/carouselDemo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,  ],
+  imports:      [ BrowserModule, FormsModule, NgCarouselModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, CarouselDemoComponent ],
   bootstrap:    [ AppComponent ]
 })
