0277b63c514de817a3e367dba380aa52740e9dff
TheBassemNabil <bass.mar111@outlook.com>
2022-02-23 19:53:39 +0200

    2 files modified, 2 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CarouselDemoComponent } from './carouselDemo/carouselDemo.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, CarouselDemoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/carouselDemo/carouselDemo.component.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carouselDemo',
+  templateUrl: './carouselDemo.component.html',
+  styleUrls: ['./carouselDemo.component.css']
+})
+export class CarouselDemoComponent implements OnInit {
+  
+  constructor(){}
+
+  ngOnInit() {
+  }
+
+}
