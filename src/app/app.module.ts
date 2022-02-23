ced433874ccc7d45fb06f08b57705f26b2f07866
TheBassemNabil <bass.mar111@outlook.com>
2022-02-23 20:54:44 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CarouselDemoComponent } from './carouselDemo/carouselDemo.component';
+import { NgCarouselModule } from 'angular-custom-carousel';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, NgCarouselModule ],
   declarations: [ AppComponent, CarouselDemoComponent ],
   bootstrap:    [ AppComponent ]
 })
